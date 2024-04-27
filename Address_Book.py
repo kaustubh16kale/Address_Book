9ec28a2e02dcd1322f6154761c398605699ee01d
Kaustubh Kale <kaustubh.mum.dbda@gmail.com>
2024-04-27 12:43:04 +0530

    [Kaustubh Kale] Add: Python script to set a relation between Address book class and contact

--- Address_Book.py
+++ Address_Book.py
@@ -1,24 +1,33 @@
 import logging
 
 logger=logging.getLogger(__name__)
-logging.basicConfig(format='%(asctime)s %(message)s',filename="Address.log",level=logging.INFO)
+logging.basicConfig(format='%(asctime)s %(message)s',
+                    filename="Address.log",
+                    level=logging.INFO)
 
-def show_details(first_name,last_name,address,city,zip,phone_number,email):
-    print(f"First name is: {first_name} ,Last name is: {last_name}, Address is : {address}, city is : {city}, zip code is : {zip}, phone number is : {phone_number}, Email is {email}")
-
-def detail_input():
-    first_name=str(input("Enter the first name  : "))
-    last_name=str(input("Enter the last name : "))
-    address=input("Enter the address : ")
-    city=input("Enter the city : ")
-    zip=input("Enter the zip code: ")
-    phone_number=int(input("Enter the phone number : "))
-    email=input("Enter the email : ")
-    show_details(first_name,last_name,address,city,zip,phone_number,email)
+class Address_Book:
+    def __init__(self):
+        self.first_name=input("Enter the first name  : ")
+        self.last_name=input("Enter the last name : ")
+        self.address=input("Enter the address : ")
+        self.city=input("Enter the city : ")
+        self.zip=int(input("Enter the zip code: "))
+        self.phone_number=int(input("Enter the phone number : "))
+        self.email=input("Enter the email : ")
+    
+    def show_details(self):
+        print(f"First name is: {self.first_name}, "
+              f"Last name is: {self.last_name}, " 
+              f"Address is : {self.address}, " 
+              f"city is : {self.city}, " 
+              f"zip code is : {zip}, " 
+              f"phone number is : {self.phone_number}, " 
+              f"Email is: {self.email}, ")
 
 def main():
     try:
-        detail_input()
+        person1=Address_Book()
+        person1.show_details()
     except Exception as e:
         logger.exception(e)
 
